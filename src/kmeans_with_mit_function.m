d2aca5a4e59cc577b0ba2e7b4d1af50a827a1f3e
Senuka Abeysinghe <58834782+Senuka291@users.noreply.github.com>
2023-11-02 08:06:38 -0400

    Update kmeans_with_mit_function.m

--- src/kmeans_with_mit_function.m
+++ src/kmeans_with_mit_function.m
@@ -1,7 +1,10 @@
+%MATLAB code 
+
 clear;
 clc;
 X_comb = [];
 
+%Genomes downloaded from NCBI database that belongs to 3 virus classes: Tobacco mosaic virus, Banana bunchy top virus, and Cauliflower mosaic virus
 genome = {'HE818416' 'HE818417' 'HE818452' 'HE818453' 'HE818454' 'MT433346' 'MT433347' 'MT433348' 'MT433349' 'MT433350' 'AB863198' 'AB863199' 'AB863200' 'AB863201' 'AB863202'}
 
 fimgs = []; 
@@ -16,11 +19,13 @@ X = transpose(X_comb);
 %Calculate for k=2,3,4
 [cidx, ctrs] = kmeans(X, 3);
 
+%Apply kmeans MATLAB function
 clust = kmeans(X,3);
 
+%Silhoutte graph is generated after using kmeans function
 [s,h] = silhouette(X,clust,'Euclidean')
 
-
+%function to generate 2D gray scale images for plant virus genomes
 function f = mit_to_img(mitochondria) 
     seq= mitochondria(1:400);
     img(500,500) = 50; %% define image
